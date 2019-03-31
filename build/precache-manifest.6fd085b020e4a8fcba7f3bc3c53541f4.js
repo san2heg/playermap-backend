46c7325d9e6377890912d7eb147288767ef50b23
Sanketh Hegde <san2heg@gmail.com>
2019-03-30 20:14:00 -0700

    Add build folder

---
+++ build/precache-manifest.6fd085b020e4a8fcba7f3bc3c53541f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "121954128ab865ee5916",
+    "url": "/static/js/main.41192e6b.chunk.js"
+  },
+  {
+    "revision": "d27335bbdd3c6afe8e57",
+    "url": "/static/js/2.7502fd36.chunk.js"
+  },
+  {
+    "revision": "121954128ab865ee5916",
+    "url": "/static/css/main.621ae584.chunk.css"
+  },
+  {
+    "revision": "54bc29052585470b497fac8310ec0c37",
+    "url": "/index.html"
+  }
+];
